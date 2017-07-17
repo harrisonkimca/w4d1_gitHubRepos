208d3b7e1e9df816774996d80d4a79d3b6de3177
hkimca <harrison.kim.ca@gmail.com>
2017-07-17 14:25:51 -0400

    Initial Commit

---
+++ 2w4d1_gitHubRepos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2w4d1_gitHubRepos
+//
+//  Created by Seantastic31 on 17/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
